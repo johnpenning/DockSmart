b761bc33f3c3b8afe40beb253d80c665f09b3e42
John Penning <jpenning@gmail.com>
2013-05-25 18:06:57 -0400

    New SettingsViewController

---
+++ BikeZen/DockSmartSettingsViewController.m
@@ -0,0 +1,38 @@
+//
+//  DockSmartSettingsViewController.m
+//  DockSmart
+//
+//  Created by John Penning on 5/25/13.
+//  Copyright (c) 2013 John Penning. All rights reserved.
+//
+
+#import "DockSmartSettingsViewController.h"
+
+@interface DockSmartSettingsViewController ()
+
+@end
+
+@implementation DockSmartSettingsViewController
+
+//- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+//{
+//    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+//    if (self) {
+//        // Custom initialization
+//    }
+//    return self;
+//}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
